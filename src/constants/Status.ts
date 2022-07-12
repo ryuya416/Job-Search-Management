09f9b4775121e061b3e357ef6f9761f04b0f4929
ryuya0416 <67462677+ryuya416@users.noreply.github.com>
2022-07-13 02:01:19 +0900

    ✨ Create Status List

---
+++ src/constants/Status.ts
@@ -0,0 +1,32 @@
+export const STATUS_LIST = [
+  {
+    id: 1,
+    color: "orange.300",
+    value: "書類選考",
+  },
+  {
+    id: 2,
+    color: "teal.300",
+    value: "一次面接",
+  },
+  {
+    id: 3,
+    color: "purple.300",
+    value: "二次面接",
+  },
+  {
+    id: 4,
+    color: "pink.300",
+    value: "最終面接",
+  },
+  {
+    id: 5,
+    color: "blue.300",
+    value: "採用",
+  },
+  {
+    id: 6,
+    color: "gray.300",
+    value: "不採用",
+  },
+];
